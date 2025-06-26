eada9fc0d30b377eac4529d61d21dab68b0585cd
Eduardo <eduardoteixeirapaulino777@gmail.com>
2025-06-25 21:05:33 -0300

    SingUp Page

---
+++ project/src/app/user/signup/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import SignUpForm from '@/components/SingupForm';
+
+export default function SignUpPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-tr from-blue-500 to-green-600 px-4">
+      <SignUpForm />
+    </div>
+  );
+}
